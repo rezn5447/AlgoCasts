cbc3e1f46cdf57a15bb37f7c2e9053eb2fb14055
Resdan Logan <rezn5447@gmail.com>
2018-01-14 13:22:14 -0800

    maxchar hungry function

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -4,7 +4,37 @@
 // --- Examples
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
+// convert str into object with frequency as values
 
-function maxChar(str) {}
+function maxChar(str) {
+  var charMap = {};
+  str.split('').map(l => charMap[l] > 0 ? charMap[l]++ : charMap[l] = 1)
+  return charMapMath.max(...Object.values(charMap))
+}
+
+
+
+function maxChar(str) {
+  var charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for(let char of str){
+    if(charMap[char]) {
+      charMap[char]++;
+    }
+    else {
+      charMap[char] = 1;
+    }
+  }
+  for ( let char in charMap){
+    if (charMap[char] > max){
+      max = charMap[char];
+      maxChar = char
+    }
+  }
+  console.log(charMap)
+  return maxChar;
+}
 
 module.exports = maxChar;
