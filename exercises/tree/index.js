987123e7af4a73ecb2b344c1cd941b5e046ed120
Resdan Logan <rezn5447@gmail.com>
2018-01-20 14:02:14 -0800

    breadth first traversal

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -34,10 +34,11 @@ class Tree {
 
   traverseBF(fn){
     const arr = [this.root];
-    while (arr.length) {
+    var result = []
+    while (arr.length){
       const node = arr.shift();
       arr.push(...node.children)
-      fn(node);
+      fun(node)
     }
   }
 }
